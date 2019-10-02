c22f88fd353b2315ddc8f6c1e6d8fd7abe9e43f0
noahl1 <you@example.com>
2019-10-02 18:01:27 -0400

    launch script

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/noah/osu-uwrt/RoboThoughts_backend/src
+# Build directory: /home/noah/osu-uwrt/RoboThoughts_backend/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gmock
+# Build directory: /home/noah/osu-uwrt/RoboThoughts_backend/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
